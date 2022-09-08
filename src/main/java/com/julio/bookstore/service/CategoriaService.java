96ea19e942e9ec5e22aeae91bbc0cd941a63bbec
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-09-08 18:50:19 -0300

    Feat: excecao personalizada para delete de categoria.

--- src/main/java/com/julio/bookstore/service/CategoriaService.java
+++ src/main/java/com/julio/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.julio.bookstore.domain.Categoria;
@@ -22,11 +23,11 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + " tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
@@ -34,7 +35,7 @@ public class CategoriaService {
 
 	public Categoria update(Integer id, CategoriaDTO objDto) {
 		Categoria obj = findById(id);
-		
+
 		obj.setNome(objDto.getNome());
 		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
@@ -42,6 +43,11 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.julio.bookstore.service.exceptions.DataIntegrityViolationException("Categoria não pode ser deteletado! possui livros associados.");
+		}
+
 	}
 }
